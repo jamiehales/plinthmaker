0bc9d60ef80f68a2a35313bcacfa23440fdffea7
Jamie <jamie@pixelballoon.com>
2026-08-08 16:39:28 -0500

    Change default support spacing to 3mm

--- src/defaults.ts
+++ src/defaults.ts
@@ -37,7 +37,7 @@ export const DEFAULT_PLINTH_ANGLE = 15
 export const DEFAULT_RAISE_BY = 3
 export const DEFAULT_SUPPORT_SIZE = 1
 export const DEFAULT_SUPPORT_TIP_SIZE = 0.5
-export const DEFAULT_SUPPORT_SPACING = 3.5
+export const DEFAULT_SUPPORT_SPACING = 3
 
 export const DEFAULT_CONE_START_GAP = 1
 export const DEFAULT_RAFT_HEIGHT = 1.5
